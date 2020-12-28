65c90dc0f97aae96d916fc4336b338bdafb52434
aermin@qq.com <aermin>
2020-12-29 00:01:34 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { WebLocks } from "./polyfill";
 
-const locks = (function() {
-    if (!navigator.locks) {
-      const webLocks = new WebLocks();
-      navigator.locks = {
-        // request: (lockName, async({name, mode}) => {})
-        request: webLocks.request,
-        // TODO：other methods
-        // query: () => ({held, pending})
-        ...navigator.locks
-      };
-    }
-    return navigator.locks;
-  })();
+const locks = (function () {
+  if (!window?.navigator?.locks) {
+    const webLocks = new WebLocks();
+    Object.defineProperty(window?.navigator, 'locks', {
+      value: {
+        request: webLocks.request
+      },
+      writable: false
+    })
+    console.log("navigator.locks~~", window.navigator.locks);
+  }
+  return window.navigator.locks;
+})();
 
-  export default locks;
+export default locks;
